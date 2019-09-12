48c8cd155f8dd42ae6c3ad49b3b229902fc2daa7
Yuhang Zhao <2546789017@qq.com>
2019-09-12 11:03:01 +0800

    Add version information.
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+    // Force Qt to use desktop OpenGL to avoid depending on ANGLE libraries.
+    QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication application(argc, argv);
+    QCoreApplication::setApplicationName(QLatin1String("Hash Calculator"));
+#ifndef Q_OS_WINDOWS
+    QCoreApplication::setApplicationVersion(QLatin1String("1.0.0"));
+#endif
+    QCoreApplication::setOrganizationName(QLatin1String("wangwenx190"));
+    QCoreApplication::setOrganizationDomain(
+        QLatin1String("wangwenx190.github.io"));
     Widget widget;
     widget.show();
     return QApplication::exec();
